c8b3965d322ec64060da33b835aa41c9a4d29b0a
Mateus <mateusjf_melo@hotmail.com>
2023-11-08 01:58:22 -0300

    css style implemented on html page

---
+++ client/js/view.js
@@ -0,0 +1,85 @@
+class View{
+    constructor(){
+        this.el = {}
+        this._loadElements()
+        this.init()
+    }
+
+    init() {
+        this.bindFormSubmit()
+        //this.bindClickMessageItems()
+        //this.bindClickNavigationBtn()
+    }
+
+    _loadElements() {
+        const ids = document.querySelectorAll('[id]')
+        ids.forEach(element => {
+            const div = document.createElement('div')
+            div.innerHTML = `<div data-${element.id}="id"></div>`
+            const idCamelCase = Object.keys(div.firstChild.dataset)[0]
+            this.el[idCamelCase] = element
+        })
+    }
+
+    async bindFormSubmit(){
+        const { submitButton } = this.el
+        submitButton.addEventListener('click', async event => {
+            event.preventDefault()
+
+            try {
+                let data = []
+                let keyword = this.el.productName.value.trim()
+                const { result } = this.el
+                keyword = keyword.replace(/\s+/g, '+')
+
+                const response = await this.request(keyword)
+                data = (await response.json()).data
+                this.updateItems(data)
+                result.innerHTML = `Resultado: ${data.length} items`
+            } catch (error) {
+                const { result, productList } = this.el
+                result.innerHTML = 'Ocorreu um erro ao tentar extrair os dados.'
+                productList.innerHTML = ''
+            }
+        })
+    }
+
+    updateItems(items){
+        let html = ''
+        items.forEach(item => {
+            const rating = item.rating.replace(' out of 5 stars', '')
+
+            const template = `
+            <li>
+                <div class="card">
+                    <div class="img-wrapper">
+                        <img src="${item.imageUrl}" />
+                    </div>
+                    <div class="content">
+                        <h2>${item.title}</h2>
+                        <p>Nota: ${rating}</p>
+                        <p>Avaliações: ${item.numReviews}</p>
+                    </div>
+                </div>
+            </li>`
+
+            html += template
+        })
+        const { productList } = this.el
+        productList.innerHTML = html
+    }
+
+    async request(keyword){
+        const url = `http://localhost:3000/api/scrape?keyword=${keyword}`
+        const response = await fetch(url, {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		})
+
+        return response
+    }
+}
+
+new View()
